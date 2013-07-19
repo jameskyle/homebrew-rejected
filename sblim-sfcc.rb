0aaacfb480739c47081039b6e124e90b23ad334f
James Kyle <james@jameskyle.org>
2013-07-19 14:34:06 -0700

    Now uses zip url.

--- sblim-sfcc.rb
+++ sblim-sfcc.rb
@@ -3,8 +3,8 @@ require 'formula'
 class SblimSfcc < Formula
   version  "6007f3b"
   homepage 'https://sourceforge.net/projects/sblim/'
-  head     'git://git.code.sf.net/p/sblim/sfcc', :using => :git, :tag => 'master'
-
+  url      'http://sourceforge.net/code-snapshots/git/s/sb/sblim/sfcc.git/sblim-sfcc-6007f3bf75a515e1f47cdbbab62fdc2f2ff981a3.zip'
+  sha1     '16c1bfc2234ec8ff35deaacda04b05eacff1993b' 
   depends_on :libtool
   depends_on :autoconf
   depends_on :automake
@@ -15,3 +15,5 @@ class SblimSfcc < Formula
     system "make", "install"
   end
 end
+
+
