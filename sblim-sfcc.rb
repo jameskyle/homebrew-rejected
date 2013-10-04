bd6ebc6ebcb14ad7f7c34dd2416172e7cc440f91
James Kyle <james@jameskyle.org>
2013-10-04 13:36:39 -0700

    Updated sblim-sfcc to current master, old sha is no longer available

--- sblim-sfcc.rb
+++ sblim-sfcc.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class SblimSfcc < Formula
-  version  "6007f3b"
+  version  "2.2.6a"
   homepage 'https://sourceforge.net/projects/sblim/'
-  url      'http://sourceforge.net/code-snapshots/git/s/sb/sblim/sfcc.git/sblim-sfcc-6007f3bf75a515e1f47cdbbab62fdc2f2ff981a3.zip'
-  sha1     '16c1bfc2234ec8ff35deaacda04b05eacff1993b' 
+  url      'http://sourceforge.net/code-snapshots/git/s/sb/sblim/sfcc.git/sblim-sfcc-274829aaad3deaeabe712ae7cf7ef6c877023e0b.zip'
+  sha1     '7f9fbd7a880e85748a017517f8f7ee1cdf55a916'
   depends_on :libtool
   depends_on :autoconf
   depends_on :automake
