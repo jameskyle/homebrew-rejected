e58e376caa69fe7c6defaf3c76b3ac96b918cf46
David Holm <dholmster@gmail.com>
2014-08-23 20:46:24 +0200

    openwsman 2.4.10

--- openwsman.rb
+++ openwsman.rb
@@ -1,9 +1,15 @@
-require 'formula'
+require "formula"
 
 class Openwsman < Formula
-  homepage 'http://openwsman.github.io'
-  url "https://github.com/Openwsman/openwsman/archive/v2.3.6.tar.gz"
-  sha1 'ed8cfc3c1705e8aecf4a64d2285e69bdfc0f11ff'
+  homepage "http://openwsman.github.io"
+  url "https://github.com/Openwsman/openwsman/archive/v2.4.10.tar.gz"
+  sha1 "c03d4d01a45fb3eac767188bd1a6ec44d0484a1d"
+
+  patch do
+    # Fix build problem when using a strict compiler, pull request #41.
+    url "https://github.com/dholm/openwsman/commit/7f38d6e.diff"
+    sha1 "303843b9628ede2ba796d06b9ea31be98b95b300"
+  end
 
   depends_on "libxml2"
   depends_on "sblim-sfcc"
@@ -14,8 +20,7 @@ class Openwsman < Formula
 
   def install
     system "./autoconfiscate.sh"
-    system "./configure",
-                          "--disable-more-warnings",
+    system "./configure", "--disable-more-warnings",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
